8f236e9e30c75d72edf04eea26d07f8206ed4a51
victoriacassolo <vitoria.cassolo@gmail.com>
2022-12-08 15:03:55 -0300

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,3 @@
-<script setup>
-import MenuIcon from '@/components/icons/menu.vue';
-import ToPayInput from '@/components/inputs/ToPayInput.vue';
-import ToBuyInput from '@/components/inputs/ToBuyInput.vue';
-import SummaryInput from '@/components/inputs/SummaryInput.vue';
-import FixedBottomButton from '@/components/buttons/FixedBottomButton.vue';
-const form = reactive({
-  brl: 0.0,
-  matic: 0.0,
-  suymmary: '',
-});
-</script>
+<script setup></script>
 
 <template>hello</template>
